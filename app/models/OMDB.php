460e12a8e416e2e65bbbd44e27d51339a8ebaafd
Kosiso Abone <abonekosiso@gmail.com>
2025-07-20 18:52:40 +0000

    created search method in model to get all movies matching a title not just one.

--- app/models/OMDB.php
+++ app/models/OMDB.php
@@ -3,6 +3,24 @@
 class OMDB {
     public function __construct() {
     }
+
+    public function search(string $query): array {
+        if (empty(OMDB_API_KEY)) {
+            echo "ERROR: OMDB_API_KEY is empty!<br>";
+            return null;
+        }
+        
+        $url = OMDB_API_URL
+             . '?apikey=' . OMDB_API_KEY
+             . '&s=' . urlencode($query);
+
+        $json = @file_get_contents($url);
+        if ($json === false) {
+            return [];
+        }
+        $data = json_decode($json, true);
+        return $data['Search'] ?? [];
+    }
     
     public function fetchByTitle(string $title): ?array {
         if (empty(OMDB_API_KEY)) {
